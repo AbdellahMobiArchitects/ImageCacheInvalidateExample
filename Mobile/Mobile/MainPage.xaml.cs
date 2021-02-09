943d041e5f0c30eeed55ea8ba3b2816f6f007836
AbdellahOusrhireDev <abdellah.ousrhire.dev@gmail.com>
2021-02-09 16:19:05 +0200

    Update

---
+++ Mobile/Mobile/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using FFImageLoading.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public string ImgUrl { get; set; } 
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            SetImage();
+        }
+
+        private async void InvalidatedImgCach_Clicked(object sender, EventArgs e)
+        {
+            await CachedImage.InvalidateCache(ImgUrl, FFImageLoading.Cache.CacheType.All);
+            SetImage();
+        }
+
+        private void SetImage()
+        {
+            ImgUrl = string.Empty;
+            ImgUrl = "https://placeimg.com/640/480/animals";
+        }
+    }
+}
